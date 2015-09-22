b412b9dd5626cd11a337ce471bba3bffa1f8c855
feit <i@feit.me>
2015-09-22 20:33:33 +0800

    fix samba permission

--- Dockerfile
+++ Dockerfile
@@ -16,18 +16,6 @@ RUN mkdir /var/run/sshd && \
     sed -ri 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config && \
     sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config
 
-# samba global config
-RUN useradd smbuser -M && \
-    sed -i 's|^\(   unix password sync = \).*|\1no|' /etc/samba/smb.conf && \
-    sed -i '/Share Definitions/,$d' /etc/samba/smb.conf && \
-    echo '   security = user' >> /etc/samba/smb.conf && \
-    echo '   directory mask = 0775' >> /etc/samba/smb.conf && \
-    echo '   force create mode = 0664' >> /etc/samba/smb.conf && \
-    echo '   force directory mode = 0775' >> /etc/samba/smb.conf && \
-    echo '   force user = smbuser' >> /etc/samba/smb.conf && \
-    echo '   force group = users' >> /etc/samba/smb.conf && \
-    echo '' >> /etc/samba/smb.conf
-
 COPY boot.sh /usr/bin/
 COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 EXPOSE 22 139 445
--- boot.sh
+++ boot.sh
@@ -1,35 +1,53 @@
 #!/usr/bin/env bash
-usage() {
-  echo ""
+usage() { local RC=${1:-0}
+  echo "should run with -e USERNAME=username -e PASSWORD=password -e PROJECT_NAME=project"
+  exit $RC
 }
 
 createuser() { local name="${1}" passwd="${2}" \
                      project="${3}" file=/etc/samba/smb.conf
+  # add system user
   useradd "$name" -m -s /bin/bash
   echo "$name ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
   echo "$name:$passwd" | chpasswd
+
+  # change samba password
   echo "$passwd" | tee - | smbpasswd -s -a "$name"
-  sed -i "/\\[$project\\]/,/^\$/d" $file
+
+  # samba global config
+  sed -i 's|^\(   unix password sync = \).*|\1no|' /etc/samba/smb.conf
+  sed -i '/Share Definitions/,$d' /etc/samba/smb.conf
+  echo "   security = user" >> $file
+  echo "   directory mask = 0775" >> $file
+  echo "   force create mode = 0664" >> $file
+  echo "   force directory mode = 0775" >> $file
+  echo "   force user = $name" >> $file
+  echo "   force group = $name" >> $file
+  echo "" >> $file
+
+  # share folder config
   echo "[$project]" >> $file
   local path="/home/$name/$project"
-  mkdir -p "$path"
+  mkdir -p "$path" && chown "$name:$name" "$path"
   echo "   path = $path" >> $file
   echo "   browseable = yes" >> $file
   echo "   read only = no" >> $file
+  echo "   writable = yes" >> $file
   echo "   guest ok = no" >> $file
   echo "   valid users = $name" >> $file
-  echo "   admin users = $name" >> $file
-  echo -e "" >> $file 
+  echo -e "" >> $file
 }
 
-cd /tmp
-while getopts "u:" opt; do
-    case "$opt" in
-        h) usage ;;
-        u) eval createuser $(sed 's/:/ /g' <<< $OPTARG) ;;
-        "?") echo "Unknown option: -$OPTARG"; usage 1 ;;
-        ":") echo "No argument value for option: -$OPTARG"; usage 2 ;;
-    esac
-done
+if [ -n "$USERNAME" ] && [ -n "$PASSWORD" ] && [ -n "$PROJECT_NAME" ]
+then
+  if id "$USERNAME" &>/dev/null
+  then
+    echo "user already create"
+  else
+    createuser $USERNAME $PASSWORD $PROJECT_NAME
+  fi
+else
+  usage 2
+fi
 
 exec supervisord
